1de3b6c2add3d856ada95fb3def4fff5a968c509
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-14 15:13:05 +0300

    page and questioncard created without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+    
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  };
+
+  const nextQuestion = () => {
+
+  };
+
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>
+        Score:
+      </p>
+      <p>
+        Loading Questions...
+      </p>
+      <QuestionCard 
+        question={''} 
+        answers={[]} 
+        callback={undefined} 
+        userAnswer={undefined} 
+        questionNr={0} 
+        totalQuestions={0}
+      />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
