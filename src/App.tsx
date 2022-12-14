505127ef7634aadf5ec1c6b49fc4caca2cac4a27
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-14 15:33:36 +0300

    data fetched and logged

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Difficulty, fetchQuizQuestions } from './API';
+
+//components
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+  
 
   const startTrivia = async () => {
     
@@ -28,14 +41,14 @@ const App = () => {
       <p>
         Loading Questions...
       </p>
-      <QuestionCard 
-        question={''} 
-        answers={[]} 
-        callback={undefined} 
-        userAnswer={undefined} 
-        questionNr={0} 
-        totalQuestions={0}
-      />
+      {/* <QuestionCard 
+        questionNr={number + 1} 
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question} 
+        answers={questions[number].answers} 
+        userAnswer={userAnswers ? userAnswers[number] : undefined} 
+        callback={checkAnswer} 
+      /> */}
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
